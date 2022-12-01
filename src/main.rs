5c7b051c427d2454993cdabb48a591f9c6ab1788
samoylenkodmitry <samoylenkodmitry@gmail.com>
2022-12-01 21:06:06 +0300

    day1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+
+mod day1;
+use crate::day1::Day1;
+
 fn main() {
-    println!("Hello, world!");
+    let input = std::fs::read_to_string("./inputs/day1.txt").unwrap();
+    let day1 = Day1;
+    println!("Day 1, part 1: {}", day1.part1(&input));
+    println!("Day 1, part 2: {}", day1.part2(&input));
 }
